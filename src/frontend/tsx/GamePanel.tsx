865ae352a16aa45ecafc3220daad688fdb062532
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2022-12-02 01:12:03 +0000

    Attempt concatenation logic

--- src/frontend/tsx/GamePanel.tsx
+++ src/frontend/tsx/GamePanel.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import useChessStore from '../ts/state/chessStore'
 import { pokeAction, resign, offerDraw, claimSpecialDraw } from '../ts/helpers/urbitChess'
-import { Side, GameID, GameInfo, ActiveGameInfo } from '../ts/types/urbitChess'
 import { CHESS } from '../ts/constants/chess'
+import { Side, GameID, SAN, GameInfo, ActiveGameInfo } from '../ts/types/urbitChess'
 
 export function GamePanel () {
   const { urbit, displayGame, displayMoves, setDisplayGame, offeredDraw, practiceBoard, setPracticeBoard } = useChessStore()
@@ -27,6 +27,17 @@ export function GamePanel () {
     await pokeAction(urbit, claimSpecialDraw(gameID))
   }
 
+  // from stackoverflow, only slightly modified to satisfy
+  // type checker
+  function sliceIntoChunks(arr: Array<SAN>, chunkSize: number) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        const chunk = arr.slice(i, i + chunkSize);
+        res.push(chunk);
+    }
+    return res;
+}
+
   return (
     <div className='game-panel-container col'>
       <div className="game-panel col">
@@ -37,15 +48,15 @@ export function GamePanel () {
           <p>{opponent}</p>
         </div>
         <div className="moves col">
-        <ul className="move-list">
+        <ol>
           {
-          Array.from(displayMoves).map((move) => {
-            return (
-              <li className="move">{move}</li>
-            )
-          })
-        }
-        </ul>
+            Array.from(sliceIntoChunks(displayMoves, 2).map(([ply1, ply2]) => {
+              return (
+                <li>{ ply1 + ' ' + ply2 }</li>
+              )
+            }))
+          }
+        </ol>
         </div>
         <div id="our-player" className={'player row' + (hasGame ? '' : ' hidden')}>
           <p>~{window.ship}</p>
