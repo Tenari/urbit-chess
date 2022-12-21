91a4bcbe30981f5a653265dc48d68fa126010355
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2022-12-21 01:09:00 +0000

    Fix displayIndex reset when clicking a game

--- src/frontend/tsx/Games.tsx
+++ src/frontend/tsx/Games.tsx
@@ -32,7 +32,7 @@ export function Games () {
                 key={key}
                 className={`game active ${colorClass} ${status}`}
                 title={gameID}
-                onClick={() => { setDisplayGame(activeGame); setDisplayIndex(activeGame.info.moves.length) }}>
+                onClick={() => { setDisplayGame(activeGame); setDisplayIndex(activeGame.info.moves.length - 1) }}>
                 <div className='row'>
                   <piece className={`game-icon ${mySide} knight`}/>
                   <div className='col game-card'>
